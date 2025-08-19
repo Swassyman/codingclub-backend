887d36fc08a6954003ab7131041fcd359626bb1d
Swassyman <swassyman2005@gmail.com>
2025-08-19 10:58:48 +0530

    some changes and deployment to vercel

--- controllers/RegistrationController.js
+++ controllers/RegistrationController.js
@@ -1,22 +1,22 @@
-    import Registration from "../models/Registration.js";
+import Registration from "../models/Registration.js";
 
-    export async function register(req, res, next) {
-    const { eventId } = req.params;
-    const userId = req.user._id;
+export async function register(req, res, next) {
+  const { eventId } = req.params;
+  const userId = req.user._id;
 
-    try {
-        const exists = await Registration.findOne({ event: eventId, user: userId });
-        if (exists) {
-        return res.status(400).json({ message: "Already registered!" });
-        }
-
-        const registration = new Registration({ event: eventId, user: userId });
-        await registration.save();
-
-        return res.status(201).json({ message: "Registered!", registration });
-    } catch (error) {
-        return res
-        .status(500)
-        .json({ message: "Registration failed", error: error.message });
+  try {
+    const exists = await Registration.findOne({ event: eventId, user: userId });
+    if (exists) {
+      return res.status(400).json({ message: "Already registered!" });
     }
-    }
+
+    const registration = new Registration({ event: eventId, user: userId });
+    await registration.save();
+
+    return res.status(201).json({ message: "Registered!", registration });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Registration failed", error: error.message });
+  }
+}
--- models/Event.js
+++ models/Event.js
@@ -1,9 +1,9 @@
-import { mongoose } from "mongoose";
+import mongoose from "mongoose";
 
 const eventSchema = new mongoose.Schema({
   eventName: { type: String, required: true },
   eventDescription: { type: String, required: true },
-  eventDate: { type: String, required: true },
+  eventDate: { type: Date, required: true },
   eventVenue: { type: String, required: true },
   eventMode: { type: String, required: true },
   eventImage: { type: String, required: false },
