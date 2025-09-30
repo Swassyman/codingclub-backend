551713eba2242d44b0a878fb046d3efff5a8b452
Swassyman <swassyman2005@gmail.com>
2025-09-30 11:46:29 +0530

    fixed mock bug

--- routes/registerRoute.js
+++ routes/registerRoute.js
@@ -2,7 +2,6 @@ import express from 'express';
 const router = express.Router();
 import { register } from '../controllers/RegistrationController.js';
 import { clerkAuth } from '../middlewares/jwtauth.js';
-import { mockAuth } from '../middlewares/mockauth.js';
 
 router.post('/:eventId', clerkAuth, register);
 
