af05ead07483d6d057c348e2b1aae47e14bc58b2
Swassy <84135882+Swassyman@users.noreply.github.com>
2025-09-30 11:54:48 +0530

    Redefine (#3)
    * checkpoint
    
    * reworked to clerk
    
    * fixed mock bug
    
    * bruh i didnt save- bug

--- routes/memberRoutes.js
+++ routes/memberRoutes.js
@@ -1,8 +1,7 @@
     import express from "express";
     const router = express.Router();
-        import { createMember, getMemberProfile} from "../controllers/MemberController.js";
+    import { createMember, getMemberProfile} from "../controllers/MemberController.js";
     import { clerkAuth } from "../middlewares/jwtauth.js";
-import { mockAuth } from "../middlewares/mockauth.js";
 
     router.post("/register", createMember);
     router.get("/profile", clerkAuth, getMemberProfile);
