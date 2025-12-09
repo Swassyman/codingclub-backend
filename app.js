2ccb35b4102ca8089ee7aa75a3e886ef0979e4f2
sreedharsanthosh <sreedharsanthsoh2005@gmail.com>
2025-12-09 10:49:09 +0530

    Fix cors allow list

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ import cookieParser from "cookie-parser";
 const app = express();
 app.use(
   cors({
-    origin: ["https://www.codingclubtkmce.in", "http://localhost:5173"],
+    origin: "https://www.codingclubtkmce.in",
     credentials: true,
   })
 );
