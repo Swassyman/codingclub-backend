63b40f83d155c2e3e717bc019bcb2cd983bae8c3
Swassyman <swassyman2005@gmail.com>
2025-12-10 17:52:17 +0530

    fix: add vercel website url to CORS

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ import cookieParser from "cookie-parser";
 const app = express();
 app.use(
   cors({
-    origin: ["https://www.codingclubtkmce.in", "http://localhost:5173"],
+    origin: ["https://www.codingclubtkmce.in", "https://club-website-bay.vercel.app"],
     credentials: true,
   })
 );
